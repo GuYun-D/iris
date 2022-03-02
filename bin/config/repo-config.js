01cc76af91490e8bf2d4a49956b7f7b948dc4407
GuYun-D <1946306647@qq.com>
2022-03-02 22:32:02 +0800

    完成下载项目逻辑

---
+++ bin/config/repo-config.js
@@ -0,0 +1,7 @@
+// 通过download-git-repo使用http路径直接下载
+// https://www.npmjs.com/package/download-git-repo
+const vueRepo = 'direct:https://github.com/coderwhy/hy-vue-temp.git'
+
+module.exports = {
+  vueRepo
+}
--- bin/core/actions.js
+++ bin/core/actions.js
@@ -1,5 +1,15 @@
-const createProjectAction = (project) => {
+const { promisify } = require('util')
+// downloadGitRepoFn 不支持使用promise，使用内置模块promisify转化，将他支持promise
+const downloadGitRepoFn = promisify(require('download-git-repo'))
+const { vueRepo } = require('../config/repo-config.js')
 
+const createProjectAction = async (project) => {
+  // clone
+  await downloadGitRepoFn(vueRepo, project, { clone: true })
+  // npm init
+
+  // npm run serve
+  // open
 }
 
 module.exports = {
