27e9db9562a7bfbb7b253e6462c0e9ec0537988e
GuYun-D <1946306647@qq.com>
2022-03-04 13:29:44 +0800

    修改模板

--- bin/config/repo-config.js
+++ bin/config/repo-config.js
@@ -1,6 +1,6 @@
 // 通过download-git-repo使用http路径直接下载
 // https://www.npmjs.com/package/download-git-repo
-const vueRepo = 'direct:git@github.com:coderwhy/hy-vue-temp.git'
+const vueRepo = 'direct:git@github.com:GuYun-D/iris-vue-template.git'
 
 module.exports = {
   vueRepo
